4a44b4f7be674b0ae016bf9f3c816a61f206aa57
Danilo Valério <danvalerio89@gmail.com>
2026-08-04 11:23:12 -0300

    chore: add and configure chakra ui provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import { Provider } from "@/components/ui/provider.tsx";
+import App from "@/App";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
+    <ChakraProvider value={defaultSystem}>
       <App />
-    </Provider>
+    </ChakraProvider>
   </StrictMode>,
 );
